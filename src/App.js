eff64ff118c431febaa9c962442364663d031bf2
Jose Andino <joseandino866@gmail.com>
2024-01-25 17:59:08 -0400

    [ADD] adding the products create component to do http post in the fastapi service and the delete method in the product component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import logo from './logo.svg';
 import {Products} from "./components/Products";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { ProductsCreate } from "./components/ProductsCreate"
 
 function App() {
-   return <Products/>;
+    return <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Products />}/>
+        <Route path="/create" element={<ProductsCreate />}/>
+      </Routes>
+    
+    </BrowserRouter>
 }
 
 export default App;
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,29 +1,65 @@
-import {Wrapper} from "./Wrapper";
+import { useEffect, useState } from "react";
+import { Wrapper } from "./Wrapper";
+import {Link} from "react-router-dom"
 
 export const Products = () => {
+    const [products, setProducts] = useState([]);
 
-    return<Wrapper>
+    useEffect(() => {
+        (async () => {
+            const response = await fetch('http://localhost:8000/products');
+            const content = await response.json()
+
+            setProducts(content);
+        })();
+    }, []);
+
+    const del = async id => {
+        if(window.confirm('Are you sure to delete this record?')){
+
+            await fetch(`http://localhost:8000/product/${id}`,{
+                method: 'DELETE',
+                    
+            });
+
+            setProducts(products.filter(p => p.pk !== id));
+        }
+
+    }
+
+    return <Wrapper>
+        <div className="pt-3 pb-2 mb-3 border-bottom">
+            <Link to={`/create`} className="btn btn-sm btn-outline-secondary">Add</Link>
+        </div>
         <div class="table-responsive">
-        <table class="table table-striped table-sm">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                    <th>Header</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1,001</td>
-                    <td>Lorem</td>
-                    <td>ipsum</td>
-                    <td>dolor</td>
-                    <td>sit</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+            <table class="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map(product => {
+                        return <tr key={product.pk}>
+                            <td>{product.pk}</td>
+                            <td>{product.name}</td>
+                            <td>{product.price}</td>
+                            <td>{product.quantity}</td>
+                            <td>
+                                <a href='#' className="btn btn-sm btn-outline-secondary"
+                                    onClick={e => del(product.pk)}
+                                >
+                                    Delete
+                                </a>
+                            </td>
+                        </tr>
+                    })}
+                </tbody>
+            </table>
+        </div>
     </Wrapper>
 }
